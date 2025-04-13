76d4c525b2e9762ae62fb696808559ade8b9bde7
chenarrd delfin <chenarrd09@gmail.com>
2025-04-13 18:49:15 +0800

    init

---
+++ ShortestRouteOptimizer/Controller/ShortestRouteOptimizerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShortestRouteOptimizer.Interface;
+
+namespace ShortestRouteOptimizer
+{
+    public class ShortestRouteOptimizerController : Controller
+    {
+        private readonly IAlgorithm _algorithm;
+        public ShortestRouteOptimizerController(IAlgorithm algorithm)
+        {
+            _algorithm = algorithm;
+            // Constructor logic can be added here if needed
+        }
+        public IActionResult Index(string from, string to)
+        {
+            var graphNode = _algorithm.GetNodes();
+            var result = _algorithm.ShortestPath(from, to, graphNode);
+            ViewBag.Nodes = graphNode;
+            ViewBag.from = from == null ? "" : from.Trim();
+            ViewBag.to = to == null ? "" : to.Trim();
+            return View(result);
+        }
+    }
+}
---
+++ UnitTesting/Test1.cs
@@ -0,0 +1,40 @@
+﻿using ShortestRouteOptimizer.Business;
+using ShortestRouteOptimizer.Data;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var algorithm = new Algorithm();
+            var graphNode = algorithm.GetNodes();
+
+
+            string fromNodeName = "A";
+            string toNodeName = "D";
+            // Act
+            var result = algorithm.ShortestPath(fromNodeName, toNodeName, graphNode);
+            // Assert
+            Xunit.Assert.NotNull(result);
+            Xunit.Assert.Equal([ "A", "B", "F", "E", "D" ], result.NodeNames);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var algorithm = new Algorithm();
+            var graphNode = algorithm.GetNodes();
+
+
+            string fromNodeName = "B";
+            string toNodeName = "E";
+            // Act
+            var result = algorithm.ShortestPath(fromNodeName, toNodeName, graphNode);
+            // Assert
+            Xunit.Assert.NotNull(result);
+            Xunit.Assert.Equal(["B", "F", "E"], result.NodeNames);
+        }
+    }
+}
